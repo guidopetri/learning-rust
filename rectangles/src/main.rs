cca5777774e9a8ba414fe613eb1e04e81615a9f9
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-15 23:29:21 +0200

    using a new method that takes an extra parameter and using an associated function that serves as a constructor

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,23 +5,23 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        self.width >= other_rect.width && self.height >= other_rect.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
     }
 }
 
 fn main() {
-    let rect1 = Rectangle {
-                    width: 30,
-                    height: 50,
-                };
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
-    println!("The are of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
-    println!("rect1 is {:#?}",rect1);
-
-    // the following is allowed because we used an immutable reference above
-    // println!("{}",rect1.width)
+    let rect4 = Rectangle::square(3);
+    println!("rect4 is: {:#?}",rect4);
 }
