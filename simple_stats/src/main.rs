5f30f4839fb740874dea0a0c8959082912f6e709
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-16 22:53:28 +0200

    create simple mode calculation with a vector of u32

--- simple_stats/src/main.rs
+++ simple_stats/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
-    let list_of_integers = vec![3,5,7,1204,38,28,180,1,2,45,2,3,5,25,3,7,6,543,4,7,3,3,4,5,62,4,2];
+    let list_of_integers = vec![32,5,7,120,38,28,180,1,2,45,2,3,5,25,3,7,6,543,4,7,3,3,4,5,7,7,7,7,7,7,7,7,62,4,2];
 
     println!("{:?}",list_of_integers);
 
@@ -9,7 +9,7 @@ fn main() {
 
     println!("median is: {}",median(&list_of_integers));
 
-    println!("mode is: {}",mode(&list_of_integers));
+    println!("mode is: {:?}",mode(&list_of_integers));
 }
 
 fn mean(x: &Vec<u32>) -> f32 {
@@ -31,6 +31,27 @@ fn median(x: &Vec<u32>) -> f32 {
     }
 }
 
-fn mode(x: &Vec<u32>) -> u32 {
-    1
+fn mode(x: &Vec<u32>) -> Vec<u32> {
+    let mut my_map = HashMap::new();
+    for number in x.iter() {
+        let count = my_map.entry(*number).or_insert(0);
+        *count +=1;
+    }
+
+    let mut max_val:u32 = 0;
+
+    for val in my_map.values() {
+        if *val > max_val {
+            max_val = *val;
+        };
+    }
+
+    let mut mode: Vec<u32> = Vec::new();
+
+    for key in my_map.keys() {
+        if my_map[key] == max_val {
+            mode.append(&mut vec![*key])
+        };
+    }
+    mode
 }
